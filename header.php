a38c79293969cf89c81aafd6cbab9806e1fb7759
Alexandru Necula <alex.lox98@gmail.com>
2019-01-14 16:59:45 +0200

    various changes

--- header.php
+++ header.php
@@ -6,9 +6,12 @@
 	<meta name="author" content="Alexandru Necula">
 	<meta name="description" content="My personal portfolio. Come and find out if I am what you're looking for!">
 	<meta property="og:image" content="images/portret.png">
+	<meta property="og:image:width" content="200px">
+	<meta property="og:image:height" content="200px">
 	<meta property="og:description" content="My personal portfolio. Come and find out if I am what you're looking for!">
 	<meta property="og:title" content="Alexandru Necula">
 	<meta name="twitter:title" content="Alexandru Necula' Portfolio">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
 	<link rel="stylesheet" type="text/css" href= <?php 
 		if(isset($realFileBasename))
