1940811880ae40536ee6379ae6ea838df66e284e
Alexandru Necula <alex.lox98@gmail.com>
2019-01-15 10:12:47 +0200

    small design adjustments

--- projects.php
+++ projects.php
@@ -11,10 +11,11 @@ $result = mysqli_query($conn, $sql);
 while($row = mysqli_fetch_assoc($result))
 {
 	echo '<a target="_blank" href="' . $row['projectLink'] . '">';
-	echo '<div class="project" style="background: url(../images/' . $row['projectImageName'] . ') no-repeat center;">';
+	echo '<div class="project" style="background: url(../images/' . $row['projectImageName'] . ') center;">';
+	echo '<div class = "additional_background">';
 	echo '<div class="project-title">' . $row['projectName'] . '</div><br/>';
 	echo '<div class="project-description">' . $row['projectDescription'] . '</div>';
-	echo '</div><hr>';
+	echo '</div></div><hr>';
 	echo '</a>';
 }
 echo '</div>';
